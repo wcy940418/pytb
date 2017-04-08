29839b6104da37355b2370f8f7301d03f1ca6d10
Chaoyue Wang <wcy940418@gmail.com>
2017-04-08 01:14:56 -0400

    Added snapshot

--- src/draw.py
+++ src/draw.py
@@ -3,6 +3,7 @@ import constants as c
 from constants import layer_boxes, classes
 import numpy as np
 import sys
+import os
 import cv2
 import colorsys
 import boxproc
@@ -13,16 +14,21 @@ def draw_rect(image, rectangle, color, thickness=1):
 	right_bottom = (int((rectangle[0] + max(rectangle[2], 0)) * image.shape[1]), int((rectangle[1] + max(rectangle[3], 0)) * image.shape[0]))
 	cv2.rectangle(image, left_top, right_bottom, color, thickness)
 
-def draw_output(image, boxes, confidences, wait=1000):
+def draw_output(image, boxes, confidences, wait=1000, mode='display', step=0, path=None):
 	img = (image * 255.0).astype(np.uint8)
 	picks = boxproc.post_process(boxes, confidences)
 	color_r = (0, 0, 255)
 	for box in picks:
 		draw_rect(img, box, color_r, 2)
-	cv2.imshow("outputs", img)
-	cv2.waitKey(wait)
+	if mode == 'display':
+		cv2.imshow("outputs", img)
+		cv2.waitKey(wait)
+	elif mode == 'save':
+		write_path = os.path.join(path, str(step) + '_output.jpg')
+		print write_path
+		cv2.imwrite(write_path, img)
 
-def draw_matches(image, default_boxes, matches, anns, wait=1000):
+def draw_matches(image, default_boxes, matches, anns, wait=1000, mode='display', step=0, path=None):
 	img = (image * 255.0).astype(np.uint8)
 	color_r = (0, 0, 255)
 	color_b = (255, 0, 0)
@@ -41,5 +47,10 @@ def draw_matches(image, default_boxes, matches, anns, wait=1000):
 
 	for (gt_box, box_id) in anns:
 		draw_rect(img, gt_box, color_g)
-	cv2.imshow("matches", img)
-	cv2.waitKey(wait)
+	if mode == 'display':
+		cv2.imshow("matches", img)
+		cv2.waitKey(wait)
+	elif mode == 'save':
+		write_path = os.path.join(path, str(step) + '_matches.jpg')
+		print write_path
+		cv2.imwrite(write_path, img)
