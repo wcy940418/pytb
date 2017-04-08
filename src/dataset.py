3888e85230be3bb88c21933de5b3286142185434
Chaoyue Wang <wcy940418@gmail.com>
2017-04-08 02:32:34 -0400

    Fixed annotations bug

--- src/dataset.py
+++ src/dataset.py
@@ -34,22 +34,25 @@ class SynthLmdb:
 				imageList.append(imagePath)
 				bbList.append(bb)
 		for imagePath, bb in zip(imageList, bbList):
+			box_list = []
 			filePath = os.path.join(self.dataDirPath, imagePath)
 			img = cv2.imread(filePath, cv2.IMREAD_COLOR)
 			height, width = img.shape[0], img.shape[1]
 			resized = cv2.resize(img, (imgW, imgH))
 			resized = np.multiply(resized, 1.0/255.0)
 			images.append(resized)
-			coord = bb[0]
-			x = float(coord[0]) / width
-			y = float(coord[1]) / height
-			w = float(coord[2]) / width
-			h = float(coord[3]) / height
-			anns.append(([x, y, w, h], bb[1]))
+			for box in bb:
+				coord = box[0]
+				x = float(coord[0]) / width
+				y = float(coord[1]) / height
+				w = float(coord[2]) / width
+				h = float(coord[3]) / height
+				box_list.append(([x, y, w, h], box[1]))
+			anns.append(box_list)
 		images = np.asarray(images)
 		return (images, anns)
 
 if __name__ == '__main__':
 	db  = SynthLmdb("../data/SynthTextLmdb", "../data/SynthText")
 	batches, anns = db.nextBatch(2)
-	# print  batches.shape, anns
+	print  batches.shape, anns
