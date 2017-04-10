1fc49d4be399f95c89f21f95931195fc9450c16f
Chaoyue Wang <wcy940418@gmail.com>
2017-04-10 02:35:38 -0400

    Fixed SynthText dataset bug

--- tool/convert_synth2lmdb.py
+++ tool/convert_synth2lmdb.py
@@ -1,10 +1,12 @@
 import scipy.io as sio
 import lmdb
 import os
-import numpy
+import numpy as np
 import json
 import sys
 import cv2
+
+violet = []
 
 def checkImageIsValid(imagePath):
 	if imagePath is None:
@@ -22,25 +24,55 @@ def writeCache(env, cache):
 		for k, v in cache.iteritems():
 			txn.put(k, v)
 
+# def bbProcess(bb):
+# 	x_top_left = bb[0][0]
+# 	x_bottom_right = bb[0][2]
+# 	y_top_left = bb[1][0]
+# 	y_bottom_right = bb[1][2]
+# 	x_top_left = bb[0][0]
+# 	x_bottom_right = bb[0][2]
+# 	y_top_left = bb[1][0]
+# 	y_bottom_right = bb[1][2]
+# 	box = []
+# 	if len(x_top_left.shape) == 0 and len(x_bottom_right.shape) == 0 and len(y_top_left.shape) == 0 and len(y_bottom_right.shape) == 0:
+# 		return box
+# 	assert x_top_left.shape[0] == x_bottom_right.shape[0] and x_top_left.shape[0] == y_top_left.shape[0] and x_top_left.shape[0] == y_bottom_right.shape[0]
+# 	for x1, y1, x2, y2 in zip(x_top_left, y_top_left, x_bottom_right, y_bottom_right):
+# 		x = max(int(x1), 0)
+# 		y = max(int(y1), 0)
+# 		w = max(int(x2 - x1), 0)
+# 		h = max(int(y2 - y1), 0)
+# 		box.append(([x, y, w, h], 0))
+# 	return box
+
+# aligned x-y axis version
 def bbProcess(bb):
-	x_top_left = bb[0][0]
-	x_bottom_right = bb[0][2]
-	y_top_left = bb[1][0]
-	y_bottom_right = bb[1][2]
 	box = []
-	if len(x_top_left.shape) == 0 and len(x_bottom_right.shape) == 0 and len(y_top_left.shape) == 0 and len(y_bottom_right.shape) == 0:
+	# print bb.shape
+	if len(bb.shape) == 2:
 		return box
-	assert x_top_left.shape[0] == x_bottom_right.shape[0] and x_top_left.shape[0] == y_top_left.shape[0] and x_top_left.shape[0] == y_bottom_right.shape[0]
-	for x1, y1, x2, y2 in zip(x_top_left, y_top_left, x_bottom_right, y_bottom_right):
-		x = max(int(x1), 0)
-		y = max(int(y1), 0)
-		w = max(int(x2 - x1), 0)
-		h = max(int(y2 - y1), 0)
-		box.append(([x, y, w, h], 0))
+	bb = np.transpose(bb, (2, 0, 1))
+	# print bb.shape
+	for wbox in bb:
+		# print wbox.shape
+		mx = np.amax(wbox, axis=1)
+		mn = np.amin(wbox, axis=1)
+		# print mx
+		# print mn
+		size = mx - mn
+		x = max(int(mn[0]), 0)
+		y = max(int(mn[1]), 0)
+		w = int(size[0])
+		h = int(size[1])
+		# assert x >= 0 and y >= 0 and w > 0 and h > 0, "x: %d, y: %d, w: %d, h: %d" % (x, y, w, h)
+		if x >= 0 and y >= 0 and w > 0 and h > 0:
+			box.append(([x, y, w, h], 0))
+		else:
+			violet.append([x, y, w, h])
 	return box
 
 
-def createDataset(outputPath, configFile, imgDir):
+def createDataset(outputPath, configFile, imgDir = None):
 	"""
 	Create LMDB dataset for CRNN training.
 
@@ -61,7 +93,7 @@ def createDataset(outputPath, configFile, imgDir):
 	wordBB = data['wordBB'][0]
 	for i in range(n):
 		image_path = img_path[i][0]
-		img = os.path.join(imgDir, image_path)
+		# img = os.path.join(imgDir, image_path)
 		if True:
 			path_key = "img-%08d" % cnt
 			cache[path_key] = str(image_path)
@@ -82,6 +114,7 @@ if __name__ == '__main__':
 	'''ro run create_dataset, use command: 
 	"python create_dataset.py <config text file path> <image files dir path>" '''
 	configFile = sys.argv[1]
-	imgDir = sys.argv[2]
+	# imgDir = sys.argv[2]
 	outputPath = './Synth_data'
-	createDataset(outputPath, configFile, imgDir)
+	createDataset(outputPath, configFile)
+	print violet
