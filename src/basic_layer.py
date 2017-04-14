Fixing gradient computation bug

src/basic_layer.py
@@ -2,8 +2,10 @@ import tensorflow as tf
 import numpy as np
 
 def weightVariable(shape):
-	initial = tf.truncated_normal(shape, stddev=0.1, name='weights')
-	return tf.Variable(initial)
+	
+	# initial = tf.truncated_normal(shape, stddev=0.1, name='weights')
+	# return tf.Variable(initial)
+	return tf.get_variable('weights', shape=shape, initializer=tf.contrib.layers.xavier_initializer())
 
 def biasVariable(shape):
 	initial = tf.constant(0.0, shape=shape, name='biases')
@@ -13,12 +15,12 @@ def maxPool(input, stride=2, kernel=2, padding='SAME', name='pool'):
 	return tf.nn.max_pool(input, ksize=[1, kernel, kernel, 1], strides=[1, stride, stride, 1], padding=padding, name=name)
 
 def conv2d(input, inputNum, outputNum, kernel=[3, 3], strides=[1, 1], padding='SAME', bn=False, trainPhase=True, name='conv2d'):
-	with tf.name_scope(name) as scope:
+	with tf.variable_scope(name) as scope:
 		W = weightVariable([kernel[0], kernel[1], inputNum, outputNum])
 		b = biasVariable([outputNum])
 		conv_out = tf.nn.conv2d(input, W, strides=[1, strides[0], strides[1], 1], padding=padding)
 		biased_out = tf.nn.bias_add(conv_out, b)
 		out = tf.nn.relu(biased_out)
 		if bn:
-			out = tf.contrib.layers.batch_norm(out, center=True, is_training=trainPhase)
+			out = tf.layers.batch_normalization(out, center=True, is_training=trainPhase)
 		return out

src/model.py
@@ -22,8 +22,8 @@ class TB:
 			with tf.name_scope('preprocess') as scope:
 				images = tf.reshape(self.input, [-1, 300, 300, 3])
 				images = images * 255.0
-				mean = tf.constant([103.939, 116.779, 123.68], dtype=tf.float32, shape=[1, 1, 1, 3], name='img_mean')
-				self.imgs = images - mean
+				mean = tf.constant([-103.939, -116.779, -123.68], dtype=tf.float32, shape=[1, 1, 1, 3], name='img_mean')
+				self.imgs = tf.add(images, mean)
 				tf.summary.image('input image', self.imgs, 3)
 			self.conv1_1 = bl.conv2d(self.imgs, 3, 64, name='conv1_1')#300
 			self.conv1_2 = bl.conv2d(self.conv1_1, 64, 64, name='conv1_2')#300
@@ -55,12 +55,12 @@ class TB:
 	def initialTextBoxLayers(self):
 		with tf.name_scope('tb_extension') as scope:
 			c_ = classes + 1
-			self.out1 = bl.conv2d(self.conv4_3, 512, layer_boxes[0] * (c_ + 4), bn=True, trainPhase=self.trainPhase, kernel=[1,5], name='out1')
-			self.out2 = bl.conv2d(self.conv7, 1024, layer_boxes[1] * (c_ + 4), bn=True, trainPhase=self.trainPhase, kernel=[1,5], name='out2')
-			self.out3 = bl.conv2d(self.conv8_2, 512, layer_boxes[2] * (c_ + 4), bn=True, trainPhase=self.trainPhase, kernel=[1,5], name='out3')
-			self.out4 = bl.conv2d(self.conv9_2, 256, layer_boxes[3] * (c_ + 4), bn=True, trainPhase=self.trainPhase, kernel=[1,5], name='out4')
-			self.out5 = bl.conv2d(self.conv10_2, 256, layer_boxes[4] * (c_ + 4), bn=True, trainPhase=self.trainPhase, kernel=[1,5], name='out5')
-			self.out6 = bl.conv2d(self.pool6, 256, layer_boxes[5] * (c_ + 4), bn=True, trainPhase=self.trainPhase, kernel=[1,1], name='out6')
+			self.out1 = bl.conv2d(self.conv4_3, 512, layer_boxes[0] * (c_ + 4), bn=False, trainPhase=self.trainPhase, kernel=[1,5], name='out1')
+			self.out2 = bl.conv2d(self.conv7, 1024, layer_boxes[1] * (c_ + 4), bn=False, trainPhase=self.trainPhase, kernel=[1,5], name='out2')
+			self.out3 = bl.conv2d(self.conv8_2, 512, layer_boxes[2] * (c_ + 4), bn=False, trainPhase=self.trainPhase, kernel=[1,5], name='out3')
+			self.out4 = bl.conv2d(self.conv9_2, 256, layer_boxes[3] * (c_ + 4), bn=False, trainPhase=self.trainPhase, kernel=[1,5], name='out4')
+			self.out5 = bl.conv2d(self.conv10_2, 256, layer_boxes[4] * (c_ + 4), bn=False, trainPhase=self.trainPhase, kernel=[1,5], name='out5')
+			self.out6 = bl.conv2d(self.pool6, 256, layer_boxes[5] * (c_ + 4), bn=False, trainPhase=self.trainPhase, kernel=[1,1], name='out6')
 	def initialOutputs(self):
 		c_ = classes + 1
 		outputs = [self.out1, self.out2, self.out3, self.out4, self.out5, self.out6]
@@ -68,8 +68,8 @@ class TB:
 		for i, out in zip(range(len(outputs)), outputs):
 			w = out.get_shape().as_list()[2]
 			h = out.get_shape().as_list()[1]
-			# out = tf.transpose(out, perm=[0, 2, 1, 3])
-			out_reshaped = tf.reshape(out, [-1,w*h*layer_boxes[i], c_ + 4])
+			out1 = tf.transpose(out, perm=[0, 2, 1, 3])
+			out_reshaped = tf.reshape(out1, [-1,w*h*layer_boxes[i], c_ + 4])
 			outlist.append(out_reshaped)
 		formatted_outs = tf.concat(outlist, 1)
 
@@ -102,11 +102,11 @@ class TB_Loss():
 		self.true_locs = true_locs
 		self.positives = positives
 		self.negatives = negatives
-		posandnegs = self.positives + self.negatives
+		posandnegs = tf.add(self.positives, self.negatives)
 		positive_sum = tf.reduce_sum(self.positives, reduction_indices=1)
-		class_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.pred_labels, labels=self.true_labels) * posandnegs
+		class_loss = tf.multiply(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.pred_labels, labels=self.true_labels), posandnegs)
 		class_loss = tf.reduce_sum(class_loss, reduction_indices=1)
-		loc_loss = tf.reduce_sum(smooth_l1(self.pred_locs - self.true_locs), reduction_indices=2) * positives
+		loc_loss = tf.multiply(tf.reduce_sum(smooth_l1(self.pred_locs - self.true_locs), reduction_indices=2), positives)
 		loc_loss = tf.reduce_sum(loc_loss, reduction_indices=1)
 		total_loss = (class_loss + loc_loss) / positive_sum
 		condition = tf.equal(positive_sum, 0)

src/trainer.py
@@ -29,15 +29,15 @@ class Conf:
 		self.trainDataSet = os.path.join('..', 'data', 'SynthTextLmdb')
 		self.auxTrainDataSet = os.path.join('..', 'data', 'SynthText')
 		# self.testDataSet = os.path.join('..', 'data', 'svt1', 'test.xml')
-		self.display = False
-		self.saveSnapShot = True
+		self.display = True
+		self.saveSnapShot = False
 		self.trainLogPath = os.path.abspath(os.path.join('..', 'model', 'train'))
 		self.snapShotPath = os.path.abspath(os.path.join('..', 'model', 'snapShot'))
 
 if __name__ == '__main__':
 	gConfig = Conf()
 	# Start a new session
-	sess = tf.InteractiveSession()
+	sess = tf.Session()
 	# Declare placeholders for graph
 	images = tf.placeholder(tf.float32, [None, image_size, image_size, 3])
 	trainPhase = tf.placeholder(tf.bool)